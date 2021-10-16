22a0a7d4f5fec1178a6c219bcda3b42570f7e168
hemant.hc <iamhemantchaudhary1@gmail.com>
2021-10-17 02:31:50 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, { useState, useEffect } from "react";
 import { getStoryID } from "./hackernewsapi";
 import { Story } from "./story";
 import "./App.css";
+import iconImage from "./y18.gif";
 export const Title = () => {
   return (
     <div className="heading">
-      <img
-        src="https://news.ycombinator.com/y18.gif"
-        alt="img"
-        className="icon"
-      />
+      <img src={iconImage} alt="img" className="icon" />
       <div className="heading-text">Hackerr News</div>
       <br></br>
       <br></br>
